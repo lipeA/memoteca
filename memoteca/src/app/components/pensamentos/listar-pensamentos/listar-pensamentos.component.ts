2eb7faddabf4b20f194e0411282d168d6d61b47d
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2024-06-07 00:33:24 -0300

    criação de componentes filhos

--- memoteca/src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ memoteca/src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
+  listaPensamentos =[
+
+    // {
+    //   conteudo: 'comunicação entre os componentes',
+    //   autoria: 'Angular',
+    //   modelo: 'modelo3'
+    // },
+    // {
+    //   conteudo: 'Passo informações para o componente filho',
+    //   autoria: 'componente filho',
+    //   modelo: 'modelo1'
+    // }
+  ]
+
+
+
   constructor() { }
 
   ngOnInit(): void {
